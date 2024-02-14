5b624f5c3111dfe2155a34ee4e9c8a995fb4f578
Adam Cummings <adam_cummings_@outlook.com>
2024-02-13 18:42:03 -0600

    added taxes and broken si vale and negative numbers

--- src/App.vue
+++ src/App.vue
@@ -4,47 +4,71 @@
       <div class="input-collection">
         <div class="input-container">
           <label for="income1">Income 1</label>
-          <input style="border-left-color: transparent" class="input" type="number" name="income1" v-model="income1">
+          <div class="money">
+            <input style="border-left-color: transparent" class="input" type="number" name="income1" v-model="income1">
+          </div>
         </div>
         <div class="input-container">
           <label for="income2">Income 2</label>
-          <input style="border-left-color: transparent" class="input" type="number" name="income2" v-model="income2">
+          <div class="money">
+            <input style="border-left-color: transparent" class="input" type="number" name="income2" v-model="income2">
+          </div>
         </div>
       </div>
       <div class="input-collection">
         <div class="input-container">
           <label for="income1">Rent</label>
-          <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[0]}`" class="input" type="number"
+          <div class="money">
+            <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[0]}`" class="input" type="number"
             name="income1" v-model="rent">
+          </div>
         </div>
-        <div class="input-container">
+        <div class="input-container disabled">
           <label for="income2">Tax</label>
-          <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[1]}`" class="input" type="number"
+          <div class="money">
+            <input disabled :style="`border-left-color: ${chartData.datasets[0].backgroundColor[1]}; color: gray;`" class="input" type="number"
             name="income2" v-model="tax">
+          </div>
         </div>
       </div>
       <div class="input-collection">
         <div class="input-container">
           <label for="income1">Bills</label>
-          <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[2]}`" class="input" type="number"
+          <div class="money">
+            <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[2]}`" class="input" type="number"
             name="income1" v-model="bills">
+          </div>
         </div>
         <div class="input-container">
           <label for="income2">Food</label>
-          <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[3]}`" class="input" type="number"
+          <div class="money">
+            <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[3]}`" class="input" type="number"
             name="income2" v-model="food">
+          </div>
         </div>
       </div>
       <div class="input-collection">
         <div class="input-container">
           <label for="income1">Eating Out</label>
-          <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[4]}`" class="input" type="number"
+          <div class="money">
+            <input :style="`border-left-color: ${chartData.datasets[0].backgroundColor[4]}`" class="input" type="number"
             name="income1" v-model="eats">
+          </div>
         </div>
-        <div class="input-container">
-          <label :style="`color: ${totalSavings === 0 ? '#ff00008c' : ''}`" for="income2">Savings</label>
-          <input disabled :style="`border-left-color: ${chartData.datasets[0].backgroundColor[5]}; color: gray; color: ${totalSavings === 0 ? '#ff00008c' : ''}`" class="input" type="number"
-            name="income2" v-model="totalSavings">
+        <div class="input-container disabled">
+          <label :style="`color: ${totalSavings <= 0 ? '#ff00008c' : ''}`" for="income2">Savings</label>
+          <div class="money">
+            <input disabled
+            :style="`border-left-color: ${ totalSavings <= 0 ? '#ff00008c' : chartData.datasets[0].backgroundColor[5]}; color: gray; color: ${totalSavings <= 0 ? '#ff00008c' : ''}`"
+            class="input" type="number" name="income2" v-model="totalSavings">
+          </div>
+        </div>
+      </div>
+      <div class="input-container">
+        <label for="si-vale">Si Vale (quantity of cards)</label>
+        <div>
+          <input
+          class="input" type="number" name="si-vale" v-model="siVale">
         </div>
       </div>
     </div>
@@ -96,6 +120,7 @@ label {
   display: flex;
   flex-direction: column;
   flex: 1;
+  position: relative;
 }
 
 .input-collection {
@@ -113,9 +138,26 @@ label {
   border: 0;
   padding: 4px;
   color: white;
-  padding-left: 8px;
-  width: 100%;
+  padding-left: 20px;
+  width: 200px;
   border-left: solid 8px;
+}
+
+.input-container > .money::before {
+  content: '$';
+  position: absolute;
+  left: 14px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: white;
+}
+
+.input-container.disabled > div::before {
+  color: gray;
+}
+
+.input-container > div {
+  position: relative;
 }
 
 input::-webkit-outer-spin-button,
@@ -136,20 +178,24 @@ import Chart from 'chart.js/auto';
 export default {
   data() {
     return {
+      lower_threshold: [0.01, 746.05, 6332.06, 11128.02, 12935.83, 15487.72, 31236.50, 49233.01, 93993.91, 125325.21, 375975.62],
+      upper_threshold: [746.04, 6332.05, 11128.01, 12935.82, 15487.71, 31236.49, 49233.00, 93993.90, 125325.20, 375975.61, 9999999999],
+      fixed_fee: [0.00, 14.32, 371.83, 893.63, 1182.88, 1640.18, 5004.12, 9236.89, 22665.17, 32691.18, 117912.32],
+      exceedance: [1.92, 6.40, 10.88, 16.00, 17.92, 21.36, 23.52, 30.00, 32.00, 34.00, 35.00],
       rent: 0,
-      tax: 0,
       bills: 0,
       food: 0,
       eats: 0,
       savings: 0,
       income1: 0,
       income2: 0,
+      siVale: 1,
       chartData: {
         labels: ['Rent', 'Tax', 'Bills', 'Food', 'Eating Out', 'Savings'],
         datasets: [{
           label: 'Monthly Outgoing Expenses',
-          data: [0, 0, 0, 0, 0, 0],
-          backgroundColor: ["#1c1c1c", "#3d3d3d", "#666666", "#E3D26F", "#5BBA6F", "#5adbff"],
+          data: [0, 0, 0, 0, 0, 0, 0],
+          backgroundColor: ["#1c1c1c", "#3d3d3d", "#666666", "#E3D26F", "#5BBA6F", "#5adbff", '#ff00008c'],
           hoverOffset: 4
         }]
       },
@@ -213,6 +259,9 @@ export default {
     income2(newValue) {
       this.updateChart();
     },
+    siVale(newValue, oldValue) {
+      return;
+    }
   },
   computed: {
     totalIncome() {
@@ -221,29 +270,45 @@ export default {
     totalExpenses() {
       return this.rent + this.tax + this.bills + this.food + this.eats;
     },
+    tax() {
+      return Math.round(this.findThresholdIndex(this.income1) + this.findThresholdIndex(this.income2));
+    },
     totalSavings() {
-      return this.totalIncome - this.totalExpenses < 0 ? 0 : this.totalIncome - this.totalExpenses;
+      return Math.round(this.totalIncome - this.totalExpenses);
+    },
+    totalFood() {
+      return Math.round(this.food - (this.siVale * 4307.68));
     }
   },
   methods: {
     updateChart() {
-      this.chartData.datasets[0].data[0] = this.rent; 
-      this.chartData.datasets[0].data[1] = this.tax; 
-      this.chartData.datasets[0].data[2] = this.bills; 
-      this.chartData.datasets[0].data[3] = this.food; 
-      this.chartData.datasets[0].data[4] = this.eats; 
-      this.chartData.datasets[0].data[5] = this.totalSavings; 
+      this.chartData.datasets[0].data[0] = this.rent;
+      this.chartData.datasets[0].data[1] = this.tax;
+      this.chartData.datasets[0].data[2] = this.bills;
+      this.chartData.datasets[0].data[3] = this.food;
+      this.chartData.datasets[0].data[4] = this.eats;
+      this.chartData.datasets[0].data[5] = this.totalSavings <= 0 ? 0 : this.totalSavings;
+      this.chartData.datasets[0].data[6] = this.totalSavings <= 0 ? this.totalSavings * -1 : 0;
       localStorage.setItem('chartData', JSON.stringify(this.chartData));
       localStorage.setItem('income1', JSON.stringify(this.income1));
       localStorage.setItem('income2', JSON.stringify(this.income2));
       this.myChart.update();
+    },
+    findThresholdIndex(num) {
+      for (let i = 0; i < this.lower_threshold.length; i++) {
+        if ((num >= this.lower_threshold[i] && num < this.upper_threshold[i]) ||
+          (num >= this.lower_threshold[i] && !this.upper_threshold[i + 1])) {
+          const totalTax = this.fixed_fee[i] + (this.exceedance[i] * (num - this.lower_threshold[i]) / 100);
+          return totalTax
+        }
+      }
+      return -1;
     }
   },
   mounted() {
     const chartData = JSON.parse(localStorage.getItem('chartData'));
     if (chartData) {
       this.rent = chartData.datasets[0].data[0];
-      this.tax = chartData.datasets[0].data[1];
       this.bills = chartData.datasets[0].data[2];
       this.food = chartData.datasets[0].data[3];
       this.eats = chartData.datasets[0].data[4];
