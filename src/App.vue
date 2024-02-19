7574c7d5a5d3ffe7dba783b8574295c2be85387f
Adam Cummings <adam_cummings_@outlook.com>
2024-02-19 00:08:57 -0600

    FINALLY worked around provide inject

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="dark ? 'dark' : ''" ref="site" class="w-full h-full">
+  <div :class="isDarkMode ? 'dark' : ''" ref="site" class="w-full h-full">
     <main class="dark:bg-slate-950 dark:text-slate-400 bg-slate-400 text-slate-950 w-full h-full p-10">
       <Nav />
       <Heading />
@@ -12,6 +12,7 @@
 import Heading from '@/components/Heading.vue'
 import Body from '@/components/Body.vue'
 import Nav from '@/components/Nav.vue'
+import { computed } from 'vue'
 export default {
   components: {
     Heading,
@@ -20,26 +21,34 @@ export default {
   },
   data() {
     return {
-      dark: false
+      isDarkMode: false,
     }
   },
   provide() {
     return {
-      isDarkMode: (isDarkMode) => { this.setTheme(isDarkMode) }
+      isDarkMode: computed(() => this.isDarkMode),
+      toggleDarkMode: this.toggleDarkMode
     }
   },
   mounted() {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      this.dark = true
+    if (localStorage.isDarkModeTheme === 'true' || (!('isDarkModeTheme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      this.isDarkMode = true
     } else {
-      this.dark = false
+      this.isDarkMode = false
     }
   },
   methods: {
     setTheme(theme) {
-      if (theme) { this.dark = theme } else { this.dark = !this.dark }
-      localStorage.theme = this.dark ? 'dark' : 'light'
-    }
+      this.isDarkMode = theme
+    },
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode
+    },
   },
+  watch: {
+    isDarkMode(newVal) {
+      newVal ? localStorage.isDarkModeTheme = 'true' : localStorage.isDarkModeTheme = 'false'
+    }
+  }
 }
 </script>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,13 @@
 <template>
-    <div class="border flex justify-between flex-col gap-5">
-        <button class="flex bg-slate-500 self-end rounded-full w-14 h-8 relative" @click="themeButton.dark = !themeButton.dark; isDarkMode(themeButton.dark)">
-            <div :class="themeButton.dark ? 'translate-x-7 mr-1' : 'translate-x-0 left-0 ml-1'" class="absolute w-6 h-6 bg-slate-200 rounded-full top-1/2 -translate-y-1/2 transition-all duration-200 dark:bg-slate-950"></div>
+    <div class="flex justify-between flex-col gap-5">
+        <button class="flex bg-slate-500 self-end rounded-full w-14 h-8 relative" @click="toggleDarkMode">
+            <div :class="isDarkMode ? 'translate-x-7 mr-1' : 'translate-x-0 left-0 ml-1'" class="absolute w-6 h-6 bg-slate-200 rounded-full top-1/2 -translate-y-1/2 transition-all duration-200 dark:bg-slate-950"></div>
         </button>
-        <h1 class="self-center text-3xl">Mexico Budgeting</h1>
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            themeButton: {
-                dark: false,
-            },
-        }
-    },
-    inject: ['isDarkMode'],
-};
+    inject: ['isDarkMode','toggleDarkMode'],
+}
 </script>
-
